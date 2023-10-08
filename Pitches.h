3100ae2db6e57f375beacde2d76a40c3625cc57e
kinto27 <120184005+kinto27@users.noreply.github.com>
2023-10-08 19:32:40 +0530

    Pitches.h

---
+++ Pitches.h
@@ -0,0 +1,14 @@
+//frequency of the notes in the fourth octave
+
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
